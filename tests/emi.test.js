e55138f6f0202e7ed408f4ca7b4b7eba2ab8ae8f
aniketgohel-dev <aniketgohelp@gmail.com>
2022-03-09 13:12:12 +0530

    s

--- tests/emi.test.js
+++ tests/emi.test.js
@@ -9,16 +9,16 @@ test('Should throw an error on negative interest rate', () => {
 })
 
 test('Should calculate correct EMI installments', () => {
-  const loan = EMI.Loan(10000, 11, 10)
+  const loan = EMI.Loan(10000, 12, 10)
 
   expect(loan).toBeDefined()
   expect(loan.installments.length).toBe(12)
-  expect(loan.installments[0].installment).toBe(955.17)
-  expect(loan.installments[12 - 1].installment).toBe(955.17)
+  expect(loan.installments[0].installment).toBe(879.16)
+  expect(loan.installments[12 - 1].installment).toBe(879.16)
   expect(loan.amount).toBe(10000)
-  expect(loan.principalSum).toBe(10955.17)
-  expect(loan.interestSum).toBe(506.92)
-  expect(loan.sum).toBe(11462.09)
+  expect(loan.principalSum).toBe(10000)
+  expect(loan.interestSum).toBe(549.89)
+  expect(loan.sum).toBe(10549.89)
 
   expect(EMI.rnd(loan.principalSum + loan.interestSum)).toBe(loan.sum)
 })
