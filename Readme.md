945e022c29500a95e3d03e2164b967d5c2112b02
aniketgohel-dev <aniketgohelp@gmail.com>
2022-03-09 12:43:22 +0530

    s

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# EMI Calculator
+
+Simple EMI Calculator as an exercise for Modern Software Development workshop.
+
+
+## License
+
+MIT
--- tests/emi.test.js
+++ tests/emi.test.js
@@ -4,21 +4,21 @@ test('Loan is defined', () => {
   expect(EMI.Loan).toBeDefined()
 })
 
-// test('Should calculate correct EMI installments', () => {
-//   const loan = EMI.Loan(10000, 12, 10)
-
-//   expect(loan).toBeDefined()
-//   expect(loan.installments.length).toBe(12)
-//   expect(loan.installments[0].installment).toBe(879.16)
-//   expect(loan.installments[12 - 1].installment).toBe(879.16)
-//   expect(loan.amount).toBe(10000)
-//   expect(loan.principalSum).toBe(10000)
-//   expect(loan.interestSum).toBe(549.89)
-//   expect(loan.sum).toBe(10549.89)
-
-//   expect(EMI.rnd(loan.principalSum + loan.interestSum)).toBe(loan.sum)
-// })
-
 test('Should throw an error on negative interest rate', () => {
   expect(() => EMI.Loan(10000, -1, 10)).toThrowError('wrong parameters: 10000 -1 10')
+})
+
+test('Should calculate correct EMI installments', () => {
+  const loan = EMI.Loan(10000, 11, 10)
+
+  expect(loan).toBeDefined()
+  expect(loan.installments.length).toBe(12)
+  expect(loan.installments[0].installment).toBe(955.17)
+  expect(loan.installments[12 - 1].installment).toBe(955.17)
+  expect(loan.amount).toBe(10000)
+  expect(loan.principalSum).toBe(10955.17)
+  expect(loan.interestSum).toBe(506.92)
+  expect(loan.sum).toBe(11462.09)
+
+  expect(EMI.rnd(loan.principalSum + loan.interestSum)).toBe(loan.sum)
 })
